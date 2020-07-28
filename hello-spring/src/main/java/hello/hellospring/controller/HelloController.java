fe9bdb0c52439ac7cbafba7b4574ea2da5d46a47
lee myung ho <dl_audgh@naver.com>
2020-07-28 13:07:15 +0900

    스프링 빌드

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.hellospring.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("hello") //get메서드
     public String hello(Model model){
-        model.addAttribute("data", "hello");
-        return "hello";
+        model.addAttribute("data","hello!");
+        return "hello"; //templates의 hello파일을 찾아서 렌더링
     }
+
 }

d9b0683581f14643b7bf9be015752f128047159a
lee myung ho <dl_audgh@naver.com>
2020-08-10 01:06:15 +0900

    8/10 스프링 부트 공부

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -10,22 +10,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController<name, hello> {
 
-    @GetMapping("hello") //get메서드
+    @GetMapping("hello") //get메서드 hello 호출시 이 메서드가 호출
     public String hello(Model model){
-        model.addAttribute("data","hello!");
+        model.addAttribute("data","hello!"); //key는 data 값은 hello
         return "hello"; //templates의 hello파일을 찾아서 렌더링
     }
+    //컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버가 화면을 찾아서 처리한다.
+    //resources : templates/ + {viweName(hello)} + .html
 
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam(value = "name") String name, Model model){
-        model.addAttribute("name",name);
+        model.addAttribute("name",name); //name 파라미터가 주소 입력한 값으로 바뀐다.
         return "hello-template"; //model(name:speing)
     }
+    //@RequestParam 어노테이션은 단일 매개변수를 전달 받을 때 사용한다.
+    //vlaue 속성은 HTTP요청 파라미터의 이름을 지정한다. URL을 처리할 때 사용한다.
+    //value = key값과 동일해야 한다??
+
     @GetMapping("hello-string")
     @ResponseBody //http에서 바디부에 이 데이터를 직접 넣어주겠다.
     public String helloString(@RequestParam("name") String name){
         return "hello" + name; // hello spring
         //내가 요청한 클라이언트에 그대로 내려감
+
     }
     @GetMapping("hello-api")
     @ResponseBody  //객체가오면 기본 디폴트가 그냥 제이슨 방식으로 데이터를 만들어서 http에 반환하겠다.
---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemverRepository.java
@@ -0,0 +1,8 @@
+package hello.hellospring.repository;
+
+import java.lang.reflect.Member;
+
+public interface MemverRepository {
+    Member save(Member member);
+
+}
